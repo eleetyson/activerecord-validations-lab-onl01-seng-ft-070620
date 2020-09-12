a8248a005bd8a5d6d2de8919290678b6e29ec2a4
Ethan <eleetyson@gmail.com>
2020-09-12 18:21:09 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ActiveRecord::Base
+  # all authors have a unique name and a ten digit number
+  validates :name, presence: true, uniqueness: true
+  validates :phone_number, length: {is: 10}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ActiveRecord::Base
+  # all posts have a title
+  # content is min 250 chars
+  # summary is max 250 chars
+  # category is either "Fiction" or "Non-Fiction"
+  # custom validation for clickbaity title
+  include ActiveModel::Validations
+  validates :title, presence: true
+  validate :title_is_clickbait
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
+# add to the errors collection if title isn't clickbait
+  def title_is_clickbait
+    if title
+      unless title.match?(/Won't Believe | Secret | Guess | Top \d+/)
+        errors.add(:title, "need a more clickbaity title")
+      end
+    end
+  end
+
 end
